7470f28835f60dbdf5ed60b36b92f498c6c7fe1d
Seri Park <parkseridev@gmail.com>
2024-11-26 20:05:22 +0900

    refactor: 계산이 큰 product.js 레이지 로딩 처리

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const script = document.createElement('script');
+            script.src = '/js/products.js';
+            document.body.appendChild(script);
+
+            observer.disconnect();
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const allProductsSection = document.getElementById('all-products');
+    if (allProductsSection) {
+        observer.observe(allProductsSection);
+    }
+});
